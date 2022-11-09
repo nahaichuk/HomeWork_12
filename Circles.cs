be5b0b41bd767c9ac669ecbf6c5fa9c6d3ffe0d9
usernamenahaichuk <gely231@mail.com>
2022-11-09 03:21:52 +0200

    HomeWork_12

---
+++ Circles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_12
+{
+    internal class Circle
+    {
+        //member variables
+        internal double Radius;
+ 
+        double GetArea()
+        {
+            return Math.Pow(Radius, 2) * 3.14;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Radius: {0}", Radius);
+              Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteCircle
+    {
+        static void Main(string[] args)
+        {
+            Circle r = new Circle();
+            r.Radius = 4.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangles.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_12
+{
+    class Triangle
+    {
+        
+        internal double Footing;
+        internal double Side;
+
+        double GetArea()
+        {
+            double Height = 0;
+            Height = Math.Sqrt(Math.Pow(Side, 2) - Math.Pow(Footing, 2));
+            return 1/2*Height*Side;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Footing: {0}", Footing);
+            Console.WriteLine("Side: {0}", Side);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    class ExecuteTriangle
+    {
+        static void Main(string[] args)
+        {
+            Triangle r = new Triangle();
+            r.Footing = 4.5;
+            r.Side = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
